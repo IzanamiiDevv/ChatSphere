c9c8d0e02198abe9b496c20c3cd88bfbec218ecd
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-03 01:08:26 +0800

    add register

--- Client/src/Component/Register.tsx
+++ Client/src/Component/Register.tsx
@@ -1,16 +1,41 @@
 import { useState } from "react";
+import './Register.css'
 
 export default function Register({addName , setState}:{addName:any,setState:any}) {
 
-    const [ name, setName ] =useState('user');
+    const [ name, setName ] = useState('');
+    const [ error, setError ] = useState<undefined | string>(undefined);
 
-    const _register = () => {
-        addName();
-        setState();
+    const register = () => {
+        if (!name || name.trim() === "") {
+            setError("Can't Enter With Empty Name");
+            return;
+        }
+        
+        addName(name);
+        setState(true);
     }
     return (
-        <div>
-            <h1>Wowo</h1>
+        <div className="login-box">
+            <h2>Enter a Name</h2>
+            <form>
+                <div className="user-box">
+                    <input type="text" required  onChange={(e)=>{
+                        setName(e.target.value);
+                    }} value={name}/>
+                    <label>Username</label>
+                </div>
+                <a onClick={()=>{
+                    register();
+                }}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+                </a>
+            </form>
+            <p id="error">{error}</p>
         </div>
     )
 }
