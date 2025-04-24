6c0513fc8d64b799753caa020f0806f5865910c6
IzanamiiDevv <rafaeloli596@gmail.com>
2025-04-24 21:52:43 +0800

    add the backend connection

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -11,16 +11,18 @@ interface Chat {
     message: string
 }
 
-const socket = io("");
+const socket = io("http://localhost:3400");
 
 const ChatBox = ({ name }: ChatBoxProps) => {
     const [ message, setMessage ] = useState("");
     const [ chats, newChat ] = useState<Chat[]>([]);
 
     useEffect(() => {
-        socket.on("createChat", (data: Chat) => {
-            newChat(prev => [...prev, data]);
+        socket.on("createChat", (data: Chat[]) => {
+            newChat(data);
         });
+
+        socket.emit("rendered", null);
 
         return () => {socket.off("createChat")}
     }, []);
@@ -29,10 +31,10 @@ const ChatBox = ({ name }: ChatBoxProps) => {
             <h1>ChatBox</h1>
             <div>{chats.map((chat, id) => {
                 return (
-                <p key={id}>{`${chat.from}[${chat.time}]: ${chat.message}`}</p>
+                <p key={id}>{`${chat.from} [${chat.time}]: ${chat.message}`}</p>
             );
             })}</div>
-            <input type="text" placeholder="Type Something" onChange={(e) => {
+            <input type="text" placeholder="Type Something" value={message} onChange={(e) => {
                 setMessage(e.target.value);
             }}/>
             <button onClick={() => {
@@ -49,6 +51,8 @@ const ChatBox = ({ name }: ChatBoxProps) => {
                     time: timeString,
                     message: message
                 });
+
+                setMessage("");
             }}>Send</button>
         </Fragment>
     )
